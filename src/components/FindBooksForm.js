66d3f844c0c1ecf5024abf3df96e5c6eee27d613
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-12-19 18:33:00 -0800

    Finish cleaning up files. Fixing indentation.

--- src/components/FindBooksForm.js
+++ src/components/FindBooksForm.js
@@ -5,7 +5,7 @@ import { withRouter, Redirect, useHistory, Link } from 'react-router-dom';
 import { useState } from 'react';
 
 
-function FindBooksForm(props){
+function FindBooksForm(props) {
 
   const history = useHistory();
 
@@ -13,7 +13,7 @@ function FindBooksForm(props){
     event.preventDefault();
     props.setCategory(event.target.filter.value);
     history.push("/results");
-  }
+  };
 
   return(
     <React.Fragment>
@@ -26,49 +26,10 @@ function FindBooksForm(props){
       </div>
     </React.Fragment>
   );
-}
+};
 
 FindBooksForm.propTypes = {
   setCategory: PropTypes.func,
-}
-
+};
 
 export default FindBooksForm;
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import PropTypes from "prop-types";
-
-
-// function FindBooksForm(props){
-
-//   function handleSettingFilter(event) {
-//     event.preventDefault();
-//     props.onSubmit(event.target.filter.value)
-//   }
-
-//   return(
-//     <React.Fragment>
-//       <div className='align-items-center'>
-//         <form onSubmit={handleSettingFilter}>
-//           <br></br>
-//           <input required type='text' name='filter'></input><br></br>
-//           <button className='btn btn-secondary' type='submit'>Search</button>    
-//         </form>
-//       </div>
-//     </React.Fragment>
-//   );
-// }
-
-// FindBooksForm.propTypes = {
-//   makeAPICall: PropTypes.func,
-// }
-
-// export default FindBooksForm;
