c00eec3ebd2bbb0a9b59f8d6c2de39dd9f8d3ba7
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-12-21 12:32:45 -0800

    Bring over checkbox code from other project. Add material UI depend.

--- src/components/FindBooksForm.js
+++ src/components/FindBooksForm.js
@@ -3,33 +3,112 @@ import PropTypes from "prop-types";
 import ReactDOM from 'react-dom';
 import { withRouter, Redirect, useHistory, Link } from 'react-router-dom';
 import { useState } from 'react';
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
+function FindBooksForm(props){
 
-function FindBooksForm(props) {
+  const history = useHistory();   
+  
+  // const [genres, setGenres] =  useState([]);
+  const [localCategories, setLocalCategories] = useState([]);
 
-  const history = useHistory();
-
-  function handleSettingFilter(event) {
-    event.preventDefault();
-    props.setCategories(event.target.filter.value);
-    history.push("/results");
+  function handleChange(event) {
+    setLocalCategories([...localCategories, event.target.name])
   };
+
+  function handleSettingFilterForAPI(event) {
+    event.preventDefault();
+    props.setCategories(localCategories)
+    history.push('/results');
+  };
+
+  console.log('LOCAL CATEGORIES', localCategories);
+  
 
   return(
     <React.Fragment>
-      <div className='align-items-center'>
-        <form onSubmit={handleSettingFilter}>
-          <br></br>
-          <input required type='text' name='filter'></input><br></br>
-          <button className='btn btn-secondary' type='submit'>Search</button>    
-        </form>
-      </div>
+      <form onSubmit={handleSettingFilterForAPI}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={localCategories.mystery}
+                type="checkbox"
+                onChange={handleChange}
+                name="mystery" />
+            }
+            label="Mystery"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={localCategories.fiction}
+                type="checkbox"
+                onChange={handleChange}
+                name="fiction" />
+            }
+            label="Fiction"
+          />
+          <button className='btn btn-secondary' type='submit'>Search</button>
+        </FormGroup>
+      </form>
     </React.Fragment>
   );
-};
+}
 
 FindBooksForm.propTypes = {
-  setCategories: PropTypes.func,
-};
+  setCategories: PropTypes.func
+}
+
 
 export default FindBooksForm;
+
+
+
+
+
+
+
+
+
+
+
+
+//OLD CODE THAT SEARCHES BY 1 FORM INPUT
+// import React from 'react';
+// import PropTypes from "prop-types";
+// import ReactDOM from 'react-dom';
+// import { withRouter, Redirect, useHistory, Link } from 'react-router-dom';
+// import { useState } from 'react';
+
+
+// function FindBooksForm(props) {
+
+//   const history = useHistory();
+
+//   function handleSettingFilter(event) {
+//     event.preventDefault();
+//     props.setCategories(event.target.filter.value);
+//     history.push("/results");
+//   };
+
+//   return(
+//     <React.Fragment>
+//       <div className='align-items-center'>
+//         <form onSubmit={handleSettingFilter}>
+//           <br></br>
+//           <input required type='text' name='filter'></input><br></br>
+//           <button className='btn btn-secondary' type='submit'>Search</button>    
+//         </form>
+//       </div>
+//     </React.Fragment>
+//   );
+// };
+
+// FindBooksForm.propTypes = {
+//   setCategories: PropTypes.func,
+// };
+
+// export default FindBooksForm;
--- src/components/FindBooksResult.js
+++ src/components/FindBooksResult.js
@@ -12,12 +12,12 @@ function FindBooksResult(props) {
     fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${props.categories}`) 
     .then((response) => response.json())
     .then((jsonifiedResponse) => {
-      setBooks(jsonifiedResponse.items.slice(0,3));
-      // setBooks(jsonifiedResponse.items);
+      // setBooks(jsonifiedResponse.items.slice(0,3));
+      setBooks(jsonifiedResponse.items);
     })
     .catch((error) => {
     });
-  };
+  }
   console.log("BOOKS ", books);
 
   return(
@@ -42,10 +42,72 @@ function FindBooksResult(props) {
       </ul>
     </React.Fragment>
   );
-};
+}
 
 FindBooksResult.propTypes = {
-  categories: PropTypes.string
-};
+  categories: PropTypes.object
+}
   
 export default FindBooksResult;
+
+
+
+
+
+
+
+
+
+//OLD CODE THAT SEARCHES BY 1 FORM INPUT
+
+// import React, { useEffect, useState } from "react";
+// import PropTypes from 'prop-types';
+
+// function FindBooksResult(props) {
+//   const [books, setBooks] = useState([]);
+
+//   useEffect(() => {
+//     makeAPICall();
+//   },[]);
+
+//   const makeAPICall = () => {
+//     fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${props.categories}`) 
+//     .then((response) => response.json())
+//     .then((jsonifiedResponse) => {
+//       setBooks(jsonifiedResponse.items.slice(0,3));
+//       // setBooks(jsonifiedResponse.items);
+//     })
+//     .catch((error) => {
+//     });
+//   };
+//   console.log("BOOKS ", books);
+
+//   return(
+//     <React.Fragment>
+//       <h1>BookResults</h1>
+//       <ul className='center-align'>
+//         {books.map((books, index) => (
+//           <div key={index} className="flip-card">
+//             <div className="flip-card-inner">
+//               <div className="flip-card-front">
+//                 <img className="dogPic" src={books.volumeInfo.imageLinks.thumbnail} alt="googleAPIImage" />
+//               </div>
+//               <li>
+//                 <div className="flip-card-back">
+//                   <p>Title: {books.volumeInfo.title}</p>
+//                   <p>Info: {books.volumeInfo.authors}</p>
+//                 </div>
+//               </li>
+//             </div>
+//           </div>
+//         ))}
+//       </ul>
+//     </React.Fragment>
+//   );
+// };
+
+// FindBooksResult.propTypes = {
+//   categories: PropTypes.string
+// };
+  
+// export default FindBooksResult;
