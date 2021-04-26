5957c1c49b42f7019fdbdca992222f75b9e0b188
Chloe Hellberg <chloe.hellberg@gmail.com>
2021-04-26 14:19:46 -0700

    Clean up empty h1.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
   return (
     <React.Fragment>
       <div className="footer-container">
         <div className="footer">
-          <h1></h1>
-        <hr/>
+          <hr />
         </div>
       </div>
     </React.Fragment>
