6a8ed4d5a31774d96f6f339c91535e44e445db2b
Chloe Hellberg <chloe.hellberg@gmail.com>
2021-04-26 14:17:30 -0700

    Fix target blank page.

--- src/components/FindBooksResult.js
+++ src/components/FindBooksResult.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 function FindBooksResult(props) {
   const [books, setBooks] = useState([]);
@@ -7,50 +7,61 @@ function FindBooksResult(props) {
 
   useEffect(() => {
     makeAPICall();
-  },[]);
+  }, []);
 
   const makeAPICall = () => {
-    console.log("CALL API", props.categories)
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${props.categories}`) 
-    .then((response) => response.json())
-    .then((jsonifiedResponse) => {
-      setBooks(jsonifiedResponse.items.slice(0,4));
-    })
-    .catch((error) => {
-    });
+    console.log("CALL API", props.categories);
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=subject:${props.categories}`
+    )
+      .then((response) => response.json())
+      .then((jsonifiedResponse) => {
+        setBooks(jsonifiedResponse.items.slice(0, 4));
+      })
+      .catch((error) => {});
   };
-  
 
-  return(
+  return (
     <React.Fragment>
-      <h1><u>BookResults</u> - you got...{props.categories}</h1>
-      <div className='bookContainer'>
-        <ul className='center-align'>
+      <h1>
+        <u>BookResults</u> - you got...{props.categories}
+      </h1>
+      <div className="bookContainer">
+        <ul className="center-align">
           {books.map((books, index) => (
             <div key={index} className="flip-card">
               <div className="flip-card-inner">
                 <div className="flip-card-front">
-                  <img className="bookPic" src={books.volumeInfo.imageLinks.thumbnail} alt="googleAPIImage" />
+                  <img
+                    className="bookPic"
+                    src={books.volumeInfo.imageLinks.thumbnail}
+                    alt="googleAPIImage"
+                  />
                 </div>
                 <li>
                   <div className="flip-card-back">
                     <p>Title: {books.volumeInfo.title}</p>
                     <p>Author(s): {books.volumeInfo.authors}</p>
-                    <a href={books.volumeInfo.previewLink} target="_blank"><button className='btn'>info</button></a>
+                    <a
+                      href={books.volumeInfo.previewLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button className="btn">info</button>
+                    </a>
                   </div>
                 </li>
               </div>
             </div>
           ))}
-          
         </ul>
       </div>
     </React.Fragment>
   );
-};
+}
 
 FindBooksResult.propTypes = {
-  categories: PropTypes.array
+  categories: PropTypes.array,
 };
-  
+
 export default FindBooksResult;
